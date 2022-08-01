ce0dac5c3f17d01c828df340f182f3561e78f12a
Nelson Pacheco <nelsonpacheco@Odd-sunday-MBP.Home>
2022-08-01 02:10:52 +0100

    Added routes and models

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const app = express();
-
 const port = process.env.PORT || 8000;
 
 app.use((req, res, next) => {
@@ -13,6 +11,11 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.send('Welcome to Express');
 });
+
+
+const userRouter = require('.routes/User');
+app.use('user', userRouter);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ Backend/routes/User.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const userRouter = exoress.Router();
+// CC
+const User = require('../models/User');
+
+// https://www.youtube.com/watch?v=uw1c4Cfl9iU
