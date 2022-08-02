1a440831ff9c3aff7bfd171db10fad7d88aadb29
Nelson Pacheco <nelsonpacheco@Odd-sunday-MBP.Home>
2022-08-03 00:48:51 +0100

    Connected db

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose")
+app.use(cors());
 const port = process.env.PORT || 8000;
+require("dotenv").config({ path: "./config.env" });
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -12,10 +16,22 @@ app.use((req, res, next) => {
   res.send('Welcome to Express');
 });
 
+// Routes
+//const userRouter = require('.routes/User');
+//app.use('user', userRouter);
 
-const userRouter = require('.routes/User');
-app.use('user', userRouter);
+//const locationRouter = require('.routes/Location');
+//app.use('location', locationRouter);
 
+
+// Connect to db
+mongoose
+    .connect(process.env.ATLAS_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    })
+    .then(() => console.log("db Connected"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
